872193b7f5a6678f8ee0b1c352814f5247d10449
NadavZ101 <nadavzivyon@gmail.com>
2024-03-10 17:04:08 +0200

    Add book preview cmp

--- cmps/BooksList.jsx
+++ cmps/BooksList.jsx
@@ -1,11 +1,13 @@
 
+import { BookPreview } from './BookPreview.jsx'
+
 export function BooksList({ books }) {
     console.log('Books List: ', books)
 
-    return <ul className="book-list">
+    return <ul className="book-list clean-list">
         {
-            books.map(book => <li key={book.id}>
-                {book.title}
+            books.map((book, idx) => <li key={book.id}>
+                <BookPreview book={book} idx={idx} />
             </li>)
         }
     </ul>
