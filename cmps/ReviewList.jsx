64b63740b7dca741e3d3619e201bd27f95054cfd
NadavZ101 <nadavzivyon@gmail.com>
2024-03-12 08:24:41 +0200

    Fix no reviews for book in ReviewList cmp

--- cmps/ReviewList.jsx
+++ cmps/ReviewList.jsx
@@ -6,13 +6,13 @@ import { Review } from "./Review.jsx"
 export function ReviewList({ book }) {
     console.log('review list - ', book)
 
-    if (!book.reviews.length) return <div>No Reviews for the book</div>
+    if (!book.reviews || !book.reviews.length) return <div>No Reviews for the book</div>
     return <ul className="review-list">
         {
             book.reviews.map(review => <li key={book.fullname}>
                 <Review review={review} />
 
-                {/* <div className="car-actions">
+                {/* <div className="review-actions">
                     <button className="remove-btn" onClick={() => onRemoveReview(book.id)}>X</button>
                 </div> */}
             </li>)
